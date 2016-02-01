5a56908936deed32f23b7ec30031a72a482ce69d
ALEX SLYE <slyealex@live.com>
2016-01-31 21:08:52 -0500

    fixed comment saving function, site fully tested and working

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,8 @@
 class ReviewsController < ApplicationController
+  before_action :load_product
   before_action :ensure_logged_in, only: [:create, :destroy]
-  
+
+
   def show
     @review = Review.find(params[:id])
   end
